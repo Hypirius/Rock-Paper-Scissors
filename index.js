019cd28f970b8849bccd9ea07806402f87870e3a
Hypirius <zuhayerhossain25@gmail.com>
2025-05-12 14:27:59 +0600

    Add initial working state

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const computerScore = 0;
-const humanScore = 0;
+let computerScore = 0;
+let humanScore = 0;
 
-function playRound() {}
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice == "scissors") {
+    computerScore++;
+  } else if (computerChoice === "rock" && humanChoice === "paper") {
+    humanScore++;
+  } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    humanScore++;
+  } else if (computerChoice === "paper" && humanChoice == "scissors") {
+    humanScore++;
+  }
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    if (!humanSelection) {
+      break;
+    }
+
+    playRound(humanSelection, computerSelection);
+  }
+
+  if (computerScore > humanScore) {
+    console.log("You lost!");
+  } else if (humanScore > computerScore) {
+    console.log("You won!");
+  }
+}
 
 function getComputerChoice() {
   const ratio = Math.random();
@@ -17,10 +50,11 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   const humanChoice = prompt("Pick your move").toLowerCase();
+  console.log(humanChoice);
 
   if (
-    humanChoice !== "rock" ||
-    humanChoice !== "paper" ||
+    humanChoice !== "rock" &&
+    humanChoice !== "paper" &&
     humanChoice !== "scissors"
   ) {
     console.log("Pick a valid move");
@@ -29,3 +63,5 @@ function getHumanChoice() {
 
   return humanChoice;
 }
+
+playGame();
